983379706bee791457a09cfb15443b8bc35aebde
Manoj Kumar Rabidas <manojkumarrabidas202@gmail.com>
2026-01-15 19:11:24 +0530

    build bug fix 1

--- CLIENT/vite.config.js
+++ CLIENT/vite.config.js
@@ -27,40 +27,42 @@
 //   },
 // });
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/server": {
-        target: "http://localhost:5001",  // Your backend port
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',
-    modulePreload: true,
-    target: 'esnext',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          dateFns: ['date-fns'],  // Separate chunk for date-fns
-          // Add other large libraries here if needed
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        "/server": {
+          target: env.VITE_API_BASE_URL || "http://localhost:5001",
+          changeOrigin: true,
         },
       },
     },
-  },
-  optimizeDeps: {
-    include: ['date-fns'],
-  },
-  resolve: {
-    alias: {
-      'date-fns/_lib/format/longFormatters': 'date-fns/esm/_lib/format/longFormatters',
+    build: {
+      outDir: 'dist',
+      modulePreload: true,
+      target: 'esnext',
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom', 'react-router-dom'],
+            dateFns: ['date-fns'],  // Separate chunk for date-fns
+            // Add other large libraries here if needed
+          },
+        },
+      },
     },
-  },
+    optimizeDeps: {
+      include: ['date-fns'],
+    },
+    resolve: {
+      alias: {
+        'date-fns/_lib/format/longFormatters': 'date-fns/esm/_lib/format/longFormatters',
+      },
+    },
+  }
 });
